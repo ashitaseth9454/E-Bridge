a8d90e6cdd27f389ca7acfee8c044995e4c3146f
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-11-21 02:02:42 +0530

    uploding notice module completed

--- app/src/main/java/com/example/e_bridge/MainActivity.java
+++ app/src/main/java/com/example/e_bridge/MainActivity.java
@@ -1,28 +1,42 @@
 package com.example.e_bridge;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+//implements View.OnClickListener
 
 public class MainActivity extends AppCompatActivity {
-    boolean valid = false;
-    private EditText sroll;
-    private EditText spass;
-    private TextView failed;
+    /*  boolean valid = false;
+      private EditText sroll;
+      private EditText spass;
+      private TextView failed;
 
 
-    private String testname = "181500145";
-    private String testpass = "12345";
-    private Button login;
-    private int counter = 5;
+      private String testname = "181500145";
+      private String testpass = "12345";
+      private Button login;
+      private int counter = 5;*/
+    CardView uploadNotice;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        uploadNotice = (CardView) findViewById(R.id.addingNotice);
+        uploadNotice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), UploadingNotices.class);
+                startActivity(intent);
+
+            }
+        });
         //connecting variables to their respective widget using id in activity main file
        /* sroll = (EditText)findViewById(R.id.username);
         //typecasting in EditText incase we see any error due to the input
@@ -68,6 +82,25 @@ public class MainActivity extends AppCompatActivity {
         });*/
 
     }
+
+    public void inter(View view) {
+        Intent intent = new Intent(getApplicationContext(), UploadingNotices.class);
+        startActivity(intent);
+    }
+
+   /* @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.addingNotice:
+                Intent intent= new Intent(getApplicationContext(),UploadingNotices.class);
+                startActivity(intent);
+                Log.i("idk","shjs");
+                break;
+
+        }
+
+    }*/
    /* private boolean checkCred(String name, String pass){
         //making it case sensitive
         if(name.equals(testname) && pass.equals(testpass)){
--- app/src/main/java/com/example/e_bridge/UploadingNotices.java
+++ app/src/main/java/com/example/e_bridge/UploadingNotices.java
@@ -1,14 +1,60 @@
 package com.example.e_bridge;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import java.io.IOException;
 
 public class UploadingNotices extends AppCompatActivity {
+    private final int REQ = 1;
+    CardView selectImage;
+    ImageView imageView;
+    Bitmap image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uploading_notices);
+
+        selectImage = (CardView) findViewById(R.id.selectImage);
+        imageView = (ImageView) findViewById(R.id.noticeImageView);
+
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            //starting mainbhi laga skte the par hamko bas ek hi bulana hai
+            @Override
+            public void onClick(View view) {
+                openGallery();
+
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent pickImage = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickImage, REQ);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            try {
+                image = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            imageView.setImageBitmap(image);
+        }
     }
 }
