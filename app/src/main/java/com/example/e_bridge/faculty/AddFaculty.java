da602e85f671926983db6b09f190999f98dc3b58
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-11-21 16:43:11 +0530

    completed AddFaculty layout file

--- app/src/main/java/com/example/e_bridge/faculty/AddFaculty.java
+++ app/src/main/java/com/example/e_bridge/faculty/AddFaculty.java
@@ -1,16 +1,20 @@
 package com.example.e_bridge.faculty;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.e_bridge.R;
 
 public class AddFaculty extends AppCompatActivity {
+    ImageView addFacultyImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_faculty);
+        addFacultyImage = (ImageView) findViewById(R.id.addFacultyImage);
+
     }
 }
