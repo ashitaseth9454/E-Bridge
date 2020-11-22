4ebe0f88ec44d1bb595dbcd6b09bacbce233c655
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-11-22 17:50:26 +0530

    Recycle r view for delete notice completed

---
+++ app/src/main/java/com/example/e_bridge/notice/DeleteNotice.java
@@ -0,0 +1,18 @@
+package com.example.e_bridge.notice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.e_bridge.R;
+
+public class DeleteNotice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_notice);
+
+
+    }
+}
